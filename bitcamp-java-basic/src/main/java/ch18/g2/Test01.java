a7ce8eb397189bb047a64af0bae7570c16778826
Sooryong <syamsyalla@naver.com>
2019-07-22 20:54:10 +0900

    1907222054

---
+++ bitcamp-java-basic/src/main/java/ch18/g2/Test01.java
@@ -0,0 +1,16 @@
+package ch18.g2;
+
+public class Test01 {
+  public static void main(String[] args) {
+
+    PaperPrinter p1 = new PaperPrinter();
+    FilmPrinter p2 = new FilmPrinter();
+    
+    display(p1, "Hellow!");
+    display(p2, "World!");
+  }
+  
+  private static void display(Printer printer, String text) {
+    printer.print(text);
+  }
+}
