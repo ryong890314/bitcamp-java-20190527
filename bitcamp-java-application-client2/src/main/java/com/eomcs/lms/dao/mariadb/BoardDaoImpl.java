d89b25802068cc49f0ab5bfab56fdb67809fa72e
Sooryong <syamsyalla@naver.com>
2019-08-11 12:43:17 +0900

    1908111243

--- bitcamp-java-application-client2/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
+++ bitcamp-java-application-client2/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
@@ -55,8 +55,27 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int update(Board board) throws Exception {
-    // TODO Auto-generated method stub
-    return 0;
+    try (Connection con = DriverManager.getConnection(
+        "jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111")) {
+      
+      try (Statement stmt = con.createStatement()) {
+        
+        // update 문장은 executeUpdate()를 사용하여 서버에 전달한다.
+        int count = stmt.executeUpdate(
+            "update lms_board set title = '" + title
+            + "', contents = '" + contents + "'"
+            + " where board_id = " + no);
+        
+        if (count == 0) {
+          System.out.println("해당 번호의 게시물이 존재하지 않습니다.");
+        } else {
+          System.out.println("변경하였습니다.");
+        }
+      }
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
