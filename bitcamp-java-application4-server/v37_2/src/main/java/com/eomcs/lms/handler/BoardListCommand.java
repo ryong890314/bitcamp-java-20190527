1ad2841fe47df3dfab135b521b063e171d5f4e3f
현수룡 <syamsyalla@naver.com>
2019-08-13 12:07:23 +0900

    1908131206

--- bitcamp-java-application4-server/v37_2/src/main/java/com/eomcs/lms/handler/BoardListCommand.java
+++ bitcamp-java-application4-server/v37_2/src/main/java/com/eomcs/lms/handler/BoardListCommand.java
@@ -2,11 +2,11 @@ package com.eomcs.lms.handler;
 
 import java.io.BufferedReader;
 import java.io.PrintStream;
+import java.util.List;
 import com.eomcs.lms.dao.BoardDao;
 import com.eomcs.lms.domain.Board;
-import com.eomcs.util.Input;
 
-public class BoardAddCommand implements Command {
+public class BoardListCommand implements Command {
 
   private BoardDao boardDao;
 
@@ -16,21 +16,25 @@ public class BoardAddCommand implements Command {
   // => 의존 객체를 강제로 설정하게 만드는 방법? 생성자를 정의하는 것이다.
   // 다음과 같이 의존 객체를 넘겨 받는 생성자를 정의하는 것이다.
 
-  public BoardAddCommand(BoardDao boardDao) {
+  public BoardListCommand(BoardDao boardDao) {
     this.boardDao = boardDao;
+
   }
 
   @Override
   public void execute(BufferedReader in, PrintStream out) {
     try {
-      Board board = new Board();
-      board.setContents(Input.getStringValue(in, out, "내용?"));
-      boardDao.insert(board);
-      System.out.println("저장하였습니다.");
+      List<Board> boards = boardDao.findAll();
+      for (Board board : boards) {
+        out.printf("%s, %s, %s, %s\n", board.getNo(), board.getContents(),
+            board.getCreatedDate(), board.getViewCount());
+      }
+
     } catch (Exception e) {
-      System.out.println("데이터를 저장에 실패했습니다.");
-      System.out.println(e.getMessage());
+      out.println("데이터 목록 조회에 실패했습니다!");
+      out.println(e.getMessage());
+
     }
   }
-}
 
+}
