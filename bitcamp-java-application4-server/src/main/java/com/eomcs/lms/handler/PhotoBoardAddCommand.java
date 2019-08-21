0947b7866ae53e492b303f0d8203dd309cd9baea
현수룡 <syamsyalla@naver.com>
2019-08-21 19:28:39 +0900

    1908211928

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/PhotoBoardAddCommand.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/PhotoBoardAddCommand.java
@@ -3,12 +3,14 @@ package com.eomcs.lms.handler;
 import java.io.BufferedReader;
 import java.io.PrintStream;
 import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.dao.PhotoFileDao;
 import com.eomcs.lms.domain.PhotoBoard;
 import com.eomcs.util.Input;
 
 public class PhotoBoardAddCommand implements Command {
 
   private PhotoBoardDao photoBoardDao;
+  private PhotoFileDao photoFileDao;
 
   // PhotoBoardandler가 사용하는 Input 객체를 반드시 설정하도록 강제해보자!
   // => Input 객체처럼 어떤 작업을 실행하기 위해 반드시 있어야 하는 객체를
@@ -16,8 +18,9 @@ public class PhotoBoardAddCommand implements Command {
   // => 의존 객체를 강제로 설정하게 만드는 방법? 생성자를 정의하는 것이다.
   // 다음과 같이 의존 객체를 넘겨 받는 생성자를 정의하는 것이다.
 
-  public PhotoBoardAddCommand(PhotoBoardDao photoBoardDao) {
+  public PhotoBoardAddCommand(PhotoBoardDao photoBoardDao, PhotoFileDao photoFileDao) {
     this.photoBoardDao = photoBoardDao;
+    this.photoFileDao = photoFileDao;
   }
 
   @Override
