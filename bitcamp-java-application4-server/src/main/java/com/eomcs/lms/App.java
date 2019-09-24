6bfde86ed54f2ec6d64a08c556aaa6c4a114c1f8
syamsyalla <syamsyalla@naver.com>
2019-09-24 19:28:10 +0900

    1909241928

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -1,11 +1,10 @@
-// v58_2 : Page Controller를 일반 클래스(Plain Old Java Object)로 전환하기
+// v59_1 : Spring WebMVC 적용하기
 package com.eomcs.lms;
 
 // 작업:
-// => Command 인터페이스 생성
-// => XxxServlet을 XxxCommand로 전환
-// => 커맨드 객체의 관리 주체를 서블릿 컨테이너에서 Spring IoC 컨테이너로 이전.
-// => DispatcherServlet 변경
+// => ContextLoaderListener 삭제
+// => DispatcherServlet 삭제
+// => RequestMappingHandlerMapping 삭제
 //
 //
 // dummy 클래스!
