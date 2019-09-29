9318517c51dbbb83bf8e6fb9e4e9484683938106
syamsyalla <syamsyalla@naver.com>
2019-09-29 20:39:40 +0900

    1909292039

--- bitcamp-java-spring-webmvc/src/main/java/bitcamp/app2/Controller04_1.java
+++ bitcamp-java-spring-webmvc/src/main/java/bitcamp/app2/Controller04_1.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller 
+@Controller
 @RequestMapping("/c04_1")
 public class Controller04_1 {
 
